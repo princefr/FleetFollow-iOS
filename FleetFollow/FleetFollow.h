1f2fae296aa03de56d0f9eaecdc02c0b39c39984
Prince ond <ondonda.prince@hotmail.fr>
2018-07-12 12:10:01 +0200

    Initial Commit

---
+++ FleetFollow/FleetFollow.h
@@ -0,0 +1,19 @@
+//
+//  FleetFollow.h
+//  FleetFollow
+//
+//  Created by prince jackes on 12/07/2018.
+//  Copyright © 2018 prince jackes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FleetFollow.
+FOUNDATION_EXPORT double FleetFollowVersionNumber;
+
+//! Project version string for FleetFollow.
+FOUNDATION_EXPORT const unsigned char FleetFollowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FleetFollow/PublicHeader.h>
+
+
